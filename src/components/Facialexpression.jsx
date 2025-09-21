9f2ee0af384aec2ce3f9e03aba1d5a1c87878e6c
Your Name <mondalsubham801@gmail.com>
2025-09-21 13:24:25 +0530

    new one full code change

--- src/components/Facialexpression.jsx
+++ src/components/Facialexpression.jsx
@@ -1,99 +1,67 @@
 import React, { useEffect, useRef } from "react";
 import * as faceapi from "@vladmandic/face-api";
 
-
 export default function FaceExpressionDetector() {
   const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+
+  const MODEL_URL = "/models";
+
+  // Load the face-api.js models
+  const loadModels = async () => {
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+  };
+
+  // Start webcam stream
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      })
+      .catch((err) => console.error("Error accessing webcam:", err));
+  };
+
+  // Run detection when button is clicked
+  const detect = async () => {
+    if (!videoRef.current) {
+      console.error("Video reference not found");
+      return;
+    }
+
+    const detections = await faceapi
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
+
+    if (!detections || detections.length === 0) {
+      console.log("No face detected");
+      return;
+    }
+
+    // Find the most probable expression
+    const expressions = detections[0].expressions;
+    let mostProbableExpression = "";
+    let highestProbability = 0;
+
+    for (const expression of Object.keys(expressions)) {
+      if (expressions[expression] > highestProbability) {
+        highestProbability = expressions[expression];
+        mostProbableExpression = expression;
+      }
+    }
+
+    console.log("Most probable expression:", mostProbableExpression);
+  };
 
   useEffect(() => {
-    const MODEL_URL = "/models";
-
-    // Load the face-api.js models
-    const loadModels = async () => {
-      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
-    };
-
-    // Start webcam stream
-    const startVideo = () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch((err) => console.error("Error accessing webcam:", err));
-    };
-
-    // Run detection when the video starts playing
-    const handleVideoPlay = () => {
-      const video = videoRef.current;
-      const canvas = canvasRef.current;
-
-      const displaySize = {
-        width: video.videoWidth,
-        height: video.videoHeight,
-      };
-
-      // Match canvas to video size
-      faceapi.matchDimensions(canvas, displaySize);
-
-      setInterval(async () => {
-        const detections = await faceapi
-          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-          .withFaceExpressions();
-          let mostprobableexpression = "";
-          let withFaceExpressions = "";
-          for (const expression of Object.keys(detections[0].expressions)) {
-            if (detections[0].expressions[expression] > mostprobableexpression) {
-                mostprobableexpression = detections[0].expressions[expression];
-                withFaceExpressions = expression;
-            }
-          }
-          console.log(mostprobableexpression);
-
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
-        const context = canvas.getContext("2d");
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        resizedDetections.forEach((detection) => {
-          const { x, y, width, height } = detection.detection.box;
-
-          // Find the dominant expression
-          const expressions = detection.expressions;
-          const maxExpression = Object.keys(expressions).reduce((a, b) =>
-            expressions[a] > expressions[b] ? a : b
-          );
-
-          // Draw rectangle around the face
-          context.beginPath();
-          context.strokeStyle = "#00FF00"; // Green rectangle
-          context.lineWidth = 3;
-          context.rect(x, y, width, height);
-          context.stroke();
-
-          // Draw the dominant expression text
-          context.fillStyle = "#00FF00";
-          context.font = "20px Arial";
-          context.fillText(maxExpression.toUpperCase(), x + 5, y - 10);
-        });
-      }, 2000);
-    };
-
-    // Load models, then start video
+    // Load models first, then start the webcam
     loadModels().then(startVideo);
-
-    // Event listener for when the video starts playing
-    if (videoRef.current) {
-      videoRef.current.addEventListener("play", handleVideoPlay);
-    }
   }, []);
 
   return (
-    <div style={{ position: "relative", width: "720px", height: "560px", margin: "auto" }}>
+    <div style={{ position: "relative", width: "720px", height: "560px" }}>
       {/* Webcam Video */}
       <video
         ref={videoRef}
@@ -107,18 +75,11 @@ export default function FaceExpressionDetector() {
           left: 0,
         }}
       />
-
-      {/* Canvas for drawing rectangle & expression */}
-      <canvas
-        ref={canvasRef}
-        width="720"
-        height="560"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
-      />
+      
+      {/* Detect button */}
+      <button onClick={detect} style={{ marginTop: "580px" }}>
+        Detect Expressions
+      </button>
     </div>
   );
 }
